e0897fce33ca5e31a0c81e0adb6404891288139d
Christian Pinto <pintochristian2@gmail.com>
2019-04-22 20:11:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,6 @@ def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
     cheese = food & cheese_types
-    cheese
+    cheese.to_s
 end
 
